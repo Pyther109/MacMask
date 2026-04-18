709f94a3cad6fbb5435ee4f9379e39e8f0ce2385
HI7557753468 <257102606+HEYYYY1292929229@users.noreply.github.com>
2026-04-19 01:34:19 +0200

    Update Macunmask.py

--- Macunmask.py
+++ Macunmask.py
@@ -1,13 +1,14 @@
 import subprocess
+import sys
 
-def unmask():
-    interface = "wlan0"
-    
+def unmask(interface):
     try:
         print(f"Restoring original MAC address for {interface}...")
         
         subprocess.run(["sudo", "ip", "link", "set", interface, "down"], check=True)
+        
         subprocess.run(["sudo", "macchanger", "-p", interface], check=True)
+        
         subprocess.run(["sudo", "ip", "link", "set", interface, "up"], check=True)
 
         print(f"Identity restored. You are no longer a ghost on {interface}. 🏠")
@@ -15,4 +16,9 @@ def unmask():
         print(f"Error during execution: {e}")
 
 if __name__ == "__main__":
-    unmask()
+    if len(sys.argv) > 1:
+        target_interface = sys.argv[1]
+    else:
+        target_interface = "wlan0"
+        
+    unmask(target_interface)
